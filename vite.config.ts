75045786c75627d76507d8ed7a80da4786628145
정현진 <jhj1004v@naver.com>
2024-04-22 15:35:55 +0900

    CHORE : vite는 케밥이 기본, 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // vite는 케밥이 기본, 설정
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
 })
