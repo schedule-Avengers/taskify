6f4b3fff17ec3469e8af54e6970dc6db234f654d
정현진 <jhj1004v@naver.com>
2024-04-26 22:02:15 +0900

    CHORE : .eslintrc 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,11 +25,14 @@ module.exports = {
   },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh'], // 'prettier' 추가
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/no-unused-vars': 'off', //대신 사용하지 않는 변수는 @typescript/eslint를 통해 잡아줌.
+    '@typescript-eslint/no-explicit-any': 'off',
   },
-}
+};
