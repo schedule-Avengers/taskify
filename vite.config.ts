e86caf37d3084735734f5e3c16c3251536ead7d4
정 현 진 <jhj1004v@naver.com>
2024-05-07 14:38:15 +0900

    CHORE : src 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -16,16 +16,15 @@ export default defineConfig({
   },
   resolve: {
     alias: [
-      { find: '@', replacement: resolve(__dirname, 'src') },
-      { find: '@axios', replacement: resolve(__dirname, 'src/axios') },
-      {
-        find: '@components',
-        replacement: resolve(__dirname, 'src/components'),
-      },
-      {
-        find: '@pages',
-        replacement: resolve(__dirname, 'src/pages'),
-      },
+      { find: '@', replacement: '/src' },
+      { find: '@axios', replacement: 'src/axios' },
+      { find: '@components', replacement: 'src/components' },
+      { find: '@hooks', replacement: 'src/hooks' },
+      { find: '@pages', replacement: 'src/pages' },
+      { find: '@stores', replacement: 'src/stores' },
+      { find: '@styles', replacement: 'src/styles' },
+      { find: '@types', replacement: 'src/types' },
+      { find: '@utils', replacement: 'src/utils' },
     ],
   },
 });
