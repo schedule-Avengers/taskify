f0cc7fb366fdebe9bc18b952f5e1ba59b2af75f1
정 현 진 <jhj1004v@naver.com>
2024-05-20 14:04:33 +0900

    DESIGN: 초기세팅 설정 후 파일 확장자 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
-import './main.css';
+import './main.module.scss';
 
 const queryClient = new QueryClient();
 
