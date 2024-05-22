5f51b1ec5703451405a3f2551537320573788fd0
정 현 진 <jhj1004v@naver.com>
2024-05-23 03:18:22 +0900

    CHORE : 로그인 페이지 HTML 작업

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,3 +1,40 @@
+import { Link } from 'react-router-dom';
+import Logo from '@/assets/svgs/logo.svg?react';
+
 export default function index() {
-  return <div></div>;
+  return (
+    <div>
+      <main>
+        <div>
+          <img />
+          <Logo />
+          <p>첫 방문을 환영합니다!</p>
+        </div>
+        <form>
+          <label htmlFor='email'>이메일</label>
+          <input id='email' placeholder='이메일을 입력해주세요' type='text' />
+          <label htmlFor='nickname'>닉네임</label>
+          <input
+            id='nickname'
+            placeholder='닉네임을 입력해 주세요'
+            type='text'
+          />
+          <label>비밀번호</label>
+          <input placeholder='8자 이상 입력해 주세요' type='password' />
+          <label>비밀번호 확인</label>
+          <input
+            placeholder='비밀번호를 한번 더 입력해 주세요'
+            type='password'
+          />
+          <input type='checkbox' id='checkbox' />
+          <label htmlFor='checkbox'>이용약관에 동의합니다.</label>
+          <button type='submit'>가입합니다.</button>
+        </form>
+        <div>
+          <p>이미 가입하셨나요?</p>
+          <Link to='/signin'>로그인하기</Link>
+        </div>
+      </main>
+    </div>
+  );
 }
