ac92aa07036038de9792d8fc2bc8040142d552fa
정 현 진 <jhj1004v@naver.com>
2024-05-23 03:15:38 +0900

    RENAME : 파일명 변경에 따른 루트 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
-import './styles/global.module.scss';
+import './styles/global.scss';
 
 const queryClient = new QueryClient();
 
